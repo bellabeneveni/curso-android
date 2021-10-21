69cb0087a78f179da9acfe121213ffaa1fc1a179
Isabella Maria Navarro Beneveni Smith <Isabella.maria@venturus.org.br>
2021-10-21 14:37:08 -0300

    Seekbar

---
+++ Seekbar/app/src/main/java/com/cursoandroid/seekbar/MainActivity.java
@@ -0,0 +1,44 @@
+package com.cursoandroid.seekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBarEscala;
+    private TextView textResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBarEscala = findViewById(R.id.seekBarEscala);
+        textResultado = findViewById(R.id.textResultado);
+
+        seekBarEscala.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                textResultado.setText("Progresso: " + progress + "/" + seekBar.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void recuperarProgresso(View view) {
+        textResultado.setText("Escolhido: " + seekBarEscala.getProgress());
+    }
+}
